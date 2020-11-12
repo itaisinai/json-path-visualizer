1c1e0e6c4912ff24f6d8e62bdca3a0bbbe7707a4
Joni <joni@altnext.com>
2020-11-12 17:14:20 +0200

    Json upload and view

---
+++ src/FileUploader/containers.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const ErrorMessage = styled.div`
+  color: red;
+  margin-top: 10px;
+`;
+
+export const DropzoneInput = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 1px solid gray;
+  border-opacity: 0.6;
+  border-radius: 5px;
+  text-align: center;
+}`
--- src/FileUploader/file-uploader.component.tsx
+++ src/FileUploader/file-uploader.component.tsx
@@ -1,5 +1,39 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from 'react';
+import Dropzone from "react-dropzone";
+import ReactJson from 'react-json-view';
+
+import { Container, DropzoneInput, ErrorMessage } from "./containers";
 
 export const FileUploader: FunctionComponent = () => {
-  return <div/>;
+  const [error, setError] = useState<string>();
+  const [json, setJson] = useState<string>();
+
+  const onDrop = (acceptedFiles: any) => {
+    if (acceptedFiles[0]) {
+      const reader = new FileReader()
+
+      reader.onabort = () => setError('file reading was aborted')
+      reader.onerror = () => setError('file reading has failed')
+      reader.onload = () => {
+        setJson(reader.result as string);
+      }
+      reader.readAsText(acceptedFiles[0])
+
+    }
+  }
+
+  return (
+    <Container>
+      <Dropzone onDrop={ onDrop } accept={ 'application/json' }>
+        { ({getRootProps, getInputProps, isDragActive, isDragReject}) => (
+          <DropzoneInput { ...getRootProps() }>
+            <input { ...getInputProps() } />
+            { isDragReject ? "File type not accepted, sorry!" : "Upload file or drop a json" }
+          </DropzoneInput>
+        ) }
+      </Dropzone>
+      <ErrorMessage>{ error }</ErrorMessage>
+      { json && <ReactJson src={ JSON.parse(json) }/> }
+    </Container>
+  );
 }
