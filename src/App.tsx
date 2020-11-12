fd75a277bfb4f68a11c9e15d7e7bca6eb4f1f3b6
Joni <joni@altnext.com>
2020-11-12 16:10:42 +0200

    Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import { FileUploader } from './FileUploader'
+import { SearchBox } from "./SearchBox";
+
+const Container = styled.div`
+`;
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+`;
+
+const JsonViewer = styled.div`
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Json Path Visualizer</Title>
+      <FileUploader />
+      <SearchBox />
+      <JsonViewer />
+    </Container>
   );
 }
 
---
+++ src/FileUploader/file-uploader.component.tsx
@@ -0,0 +1,5 @@
+import React, { FunctionComponent } from "react";
+
+export const FileUploader: FunctionComponent = () => {
+  return <div/>;
+}
